2dc9e55313965d33320c9b278972065b3ed5d914
DELL <sonipaty98@hotmail.es>
2018-01-31 17:39:53 -0500

    sonia prendiendo a crear un repo
    lo logro

---
+++ PruebaOne/src/pruebaone/PruebaOne.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaone;
+
+/**
+ *
+ * @author DELL
+ */
+public class PruebaOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
